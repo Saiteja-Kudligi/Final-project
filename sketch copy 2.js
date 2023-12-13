4d1da836ba6807c175c7e6fb27abda6240f741f1
Saiteja-Kudligi <149548893+Saiteja-Kudligi@users.noreply.github.com>
2023-12-12 19:35:56 -0800

    Add files via upload

--- sketch copy 2.js
+++ sketch copy 2.js
@@ -13,27 +13,31 @@ let sadEmoji;
 let happyEmoji;
 let surpriseEmoji;
 
-let sad, happy, surprise, angry, neutral;
+let emoji_sad, emoji_happy, emoji_surprise, emoji_angry, emoji_neutral;
 let rat_sad, rat_happy, rat_surprise, rat_angry, rat_neutral;
 let cat_sad, cat_happy, cat_surprise, cat_angry, cat_neutral;
 let dog_sad, dog_happy, dog_surprise, dog_angry, dog_neutral;
 
 let emotionsLabels = ["sad", "happy", "surprise", "angry", "neutral"];
-let selectedCharacter = "emoji";
+let selectedCharacter = "cat";
 
 let characterDropdown;
+// let ratImage;
+// let emojiImage;
+// let catImage;
+// let dogImage;
 
 function preload() {
-  ratImage = loadImage("assets/rat.png");
-  emojiImage = loadImage("assets/happy.png");
-  catImage = loadImage("assets/cat.png");
-  dogImage = loadImage("assets/dog.png");
+  // ratImage = loadImage("assets/rat.png");
+  // emojiImage = loadImage("assets/happy.png");
+  // catImage = loadImage("assets/cat.png");
+  // dogImage = loadImage("assets/dog.png");
 
-  sad = loadImage("assets/sad.png");
-  happy = loadImage("assets/happy.png");
-  surprise = loadImage("assets/surprise.png");
-  angry = loadImage("assets/angry.png");
-  neutral = loadImage("assets/neutral.png");
+  emoji_sad = loadImage("assets/sad.png");
+  emoji_happy = loadImage("assets/happy.png");
+  emoji_surprise = loadImage("assets/surprise.png");
+  emoji_angry = loadImage("assets/angry.png");
+  emoji_neutral = loadImage("assets/neutral.png");
 
   rat_sad = loadImage("assets/rat_sad.png");
   rat_happy = loadImage("assets/rat_happy.png");
@@ -54,7 +58,7 @@ function preload() {
   dog_neutral = loadImage("assets/dog_neutral.png");
 
   emotions = [
-    [sad, happy, surprise, angry, neutral],
+    [emoji_sad, emoji_happy, emoji_surprise, emoji_angry, emoji_neutral],
     [rat_sad, rat_happy, rat_surprise, rat_angry, rat_neutral],
     [cat_sad, cat_happy, cat_surprise, cat_angry, cat_neutral],
     [dog_sad, dog_happy, dog_surprise, dog_angry, dog_neutral],
@@ -68,20 +72,21 @@ function setup() {
   canvas.id("canvas");
 
   video = createCapture(VIDEO);
-  video.size(720, 480);
+  video.size(640, 480);
   video.position(windowWidth / 3.5, 130);
 
   button = createButton("Click me to toggle video");
   button.position(width, height * 1.3);
   button.mousePressed(toggleVideoCanvas);
-
+  
   //Dropdown
   characterDropdown = createSelect();
-  characterDropdown.position(10, 480);
-  characterDropdown.option("rat");
+  
+  characterDropdown.position(20, 150);
+  characterDropdown.option("cat");
   characterDropdown.option("dog");
   characterDropdown.option("emoji");
-  characterDropdown.option("cat");
+  characterDropdown.option("rat");
   
   characterDropdown.changed(() => {
     selectedCharacter = characterDropdown.value();
@@ -96,9 +101,15 @@ function setup() {
   faceapi.detect(myVideo);
 }
 
+function draw(){
+  color("white");
+  textSize(14);
+  text("Select a character:", 12, 12);
+}
+
 function ready() {
   faceapi.detect(myVideo);
-}
+  }
 
 function myVideo(error, result) {
   if (error) {
@@ -122,76 +133,76 @@ function drawCam(Video) {
 
       if (selectedCharacter == "cat") {
         if (neutral >= 0.9) {
-          image(cat_neutral, 20, 20, 100, 100);
+          image(cat_neutral, 20, height/3, 100, 100);
         }
         if (happy >= 0.7) {
-          image(cat_happy, 20, 20, 100, 100);
+          image(cat_happy, 20, height/3, 100, 100);
         }
         if (sad >= 0.5) {
-          image(cat_sad, 20, 20, 100, 100);
+          image(cat_sad, 20, height/3, 100, 100);
         }
         if (surprised >= 0.7) {
-          image(cat_surprise, 20, 20, 100, 100);
+          image(cat_surprise, 20, height/3, 100, 100);
         }
         if (angry >= 0.3) {
-          image(cat_angry, 20, 20, 100, 100);
+          image(cat_angry, 20, height/3, 100, 100);
         }
       }
 
       if (selectedCharacter == "rat") {
         if (neutral >= 0.9) {
-          image(rat_neutral, 20, 20, 100, 100);
+          image(rat_neutral, 20, height/3, 100, 100);
         }
         if (happy >= 0.7) {
-          image(rat_happy, 20, 20, 100, 100);
+          image(rat_happy, 20, height/3, 100, 100);
         }
         if (sad >= 0.5) {
-          image(rat_sad, 20, 20, 100, 100);
+          image(rat_sad, 20, height/3, 100, 100);
         }
         if (surprised >= 0.7) {
-          image(rat_surprise, 20, 20, 100, 100);
+          image(rat_surprise, 20, height/3, 100, 100);
         }
         if (angry >= 0.3) {
-          image(rat_angry, 20, 20, 100, 100);
+          image(rat_angry, 20, height/3, 100, 100);
         }
       }
 
       if (selectedCharacter == "dog") {
         if (neutral >= 0.9) {
-          image(dog_neutral, 20, 20, 100, 100);
+          image(dog_neutral, 20, height/3, 100, 100);
         }
         if (happy >= 0.7) {
-          image(dog_happy, 20, 20, 100, 100);
+          image(dog_happy, 20, height/3, 100, 100);
         }
         if (sad >= 0.5) {
-          image(dog_sad, 20, 20, 100, 100);
+          image(dog_sad, 20, height/3, 100, 100);
         }
         if (surprised >= 0.7) {
-          image(dog_surprise, 20, 20, 100, 100);
+          image(dog_surprise, 20, height/3, 100, 100);
         }
         if (angry >= 0.3) {
-          image(dog_angry, 20, 20, 100, 100);
+          image(dog_angry, 20, height/3, 100, 100);
         }
       }
 
-      // if (selectedCharacter == "emoji") {
-      //   if (neutral >= 0.7) {
-      //     image(neutral, 20, 20, 100, 100);
-      //   }
-      //   if (happy >= 0.7) {
-      //     image(happy, 20, 20, 100, 100);
-      //   }
-      //   if (sad >= 0.5) {
-      //     image(sad, 20, 20, 100, 100);
-      //   }
-      //   if (surprised >= 0.7) {
-      //     image(surprise, 20, 20, 100, 100);
-      //   }
-      //   if (angry >= 0.3) {
-      //     image(angry, 20, 20, 100, 100);
-      //   }
-      //   console.log(sadEmoji);
-      // }
+      if (selectedCharacter == "emoji") {
+        if (neutral >= 0.7) {
+          image(emoji_neutral, 20, height/3, 100, 100);
+        }
+        if (happy >= 0.7) {
+          image(emoji_happy, 20, height/3, 100, 100);
+        }
+        if (sad >= 0.5) {
+          image(emoji_sad, 20, height/3, 100, 100);
+        }
+        if (surprised >= 0.7) {
+          image(emoji_surprise, 20, height/3, 100, 100);
+        }
+        if (angry >= 0.3) {
+          image(emoji_angry, 20, height/3, 100, 100);
+        }
+        // console.log(sadEmoji);
+      }
     }
   }
 }
